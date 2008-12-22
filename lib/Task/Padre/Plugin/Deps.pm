1e8c4ab2ddfb8cea8d559b2b8fad2de593eff88f
Fayland 林 <fayland@gmail.com>
2008-12-22 02:32:14 +0000

    Import of FAYLAND/Task-Padre-Plugin-Deps-0.05 from CPAN.
    gitpan-cpan-distribution: Task-Padre-Plugin-Deps
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         FAYLAND/Task-Padre-Plugin-Deps-0.05.tar.gz
    gitpan-cpan-author:       FAYLAND
    gitpan-cpan-maturity:     released

--- lib/Task/Padre/Plugin/Deps.pm
+++ lib/Task/Padre/Plugin/Deps.pm
@@ -3,18 +3,18 @@ package Task::Padre::Plugin::Deps;
 use warnings;
 use strict;
 
+our $VERSION = '0.05';
+
+1;
+__END__
+
 =head1 NAME
 
 Task::Padre::Plugin::Deps - prereqs of Padre::Plugins
 
 =head1 VERSION
 
-Version 0.04
-
-=cut
-
-our $VERSION = '0.04';
-
+Version 0.05
 
 =head1 SYNOPSIS
 
@@ -22,20 +22,15 @@ our $VERSION = '0.04';
 
 Gabor Szabo, C<< <szabgab at gmail.com> >>
 
-
 =head1 SUPPORT
 
 L<http://padre.perlide.org/>
 
-
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2008 Gabor Szabo, all rights reserved.
+Copyright 2008 Gabor Szabo
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
-
 =cut
-
-1;
