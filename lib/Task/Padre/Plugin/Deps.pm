5f25b12eb29725381cab8ab8c82784b9f10018da
Fayland 林 <fayland@gmail.com>
2009-03-04 01:32:14 +0000

    Import of FAYLAND/Task-Padre-Plugin-Deps-0.08 from CPAN.
    gitpan-cpan-distribution: Task-Padre-Plugin-Deps
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         FAYLAND/Task-Padre-Plugin-Deps-0.08.tar.gz
    gitpan-cpan-author:       FAYLAND
    gitpan-cpan-maturity:     released

--- lib/Task/Padre/Plugin/Deps.pm
+++ lib/Task/Padre/Plugin/Deps.pm
@@ -3,7 +3,7 @@ package Task::Padre::Plugin::Deps;
 use warnings;
 use strict;
 
-our $VERSION = '0.05';
+our $VERSION = '0.08';
 
 1;
 __END__
@@ -14,7 +14,7 @@ Task::Padre::Plugin::Deps - prereqs of Padre::Plugins
 
 =head1 VERSION
 
-Version 0.05
+Version 0.08
 
 =head1 SYNOPSIS
 
